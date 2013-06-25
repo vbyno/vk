d139a50743795193888d399c217fb1788f15199e
Volodymyr Byno <volodymyr.byno@gmail.com>
2013-06-25 21:30:19 +0300

    Rails 4.0 project created. Gemfile and database.yml modificated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vk4::Application.config.secret_key_base = 'dd2da6836002254cac82d1a02a563871344cf761285a04055158734cd631c796c7688f6934d453bdf876d6e74a690bc79c8f8d71992bba8b0032d25e136535e5'
