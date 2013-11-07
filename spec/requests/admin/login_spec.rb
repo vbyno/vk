ab575ff5cf8a870a8637086a724b67437333bbb5
Volodymyr Byno <volodymyr.byno@gmail.com>
2013-11-07 08:36:54 +0200

    Activeadmin to russian language

--- spec/requests/admin/login_spec.rb
+++ spec/requests/admin/login_spec.rb
@@ -1,3 +1,4 @@
+
 require 'acceptance_helper'
 
 describe 'admin/login' do
@@ -5,9 +6,9 @@ describe 'admin/login' do
     admin = create :admin_user
     visit new_admin_user_session_path
     fill_in 'Email', with: admin.email
-    fill_in 'Password', with: admin.password
-    click_on 'Login'
+    fill_in 'Пароль', with: admin.password
+    click_on 'Войти'
 
-    expect(page).to have_content 'Dashboard'
+    expect(page).to have_content 'Панель управления'
   end
 end
