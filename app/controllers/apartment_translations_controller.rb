68b1a3c42857c5ab9ef3015ec49b7ac8de4b034b
Volodymyr Byno <volodymyr.byno@gmail.com>
2013-11-23 21:15:50 +0200

    Deleted unnecessary controller methods and templates on public site

--- app/controllers/apartment_translations_controller.rb
+++
@@ -1,3 +0,0 @@
-class ApartmentTranslationsController < InheritedResources::Base
-  before_action :authenticate_admin_user!, except: [:index, :show]
-end
--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,41 +1,9 @@
 class ApartmentsController < ApplicationController
-  before_action :authenticate_admin_user!, except: [:index, :show]
-  load_and_authorize_resource except: [:create]
+  load_and_authorize_resource only: :show
 
   def index;
     @apartments = ApartmentHandler.all(@locale)
   end
 
   def show; end
-
-  def new; end
-
-  def edit; end
-
-  def create
-    @apartment = Apartment.new(apartment_params)
-    if @apartment.save
-      redirect_to @apartment, notice: 'Apartment was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  def update
-    if @apartment.update(apartment_params)
-      redirect_to @apartment, notice: 'Apartment was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
-  def destroy
-    @apartment.destroy
-    redirect_to apartments_path
-  end
-
-private
-  def apartment_params
-    params.require(:apartment).permit(:title, :price, :description)
-  end
 end
