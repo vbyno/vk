4ebc25db76ef017e8cf9ca0c568fd249ad2854dd
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-02-17 23:24:56 +0200

    move Redirect module to initializers

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,3 @@
-module Redirect
-  extend self
-  delegate :each, to: :redirects
-
-  def node
-    @file ||=
-      YAML.load_file(Rails.root.join('config', 'redirects.yml')).with_indifferent_access
-  end
-
-  def redirects
-    (node[:children]).inject({}) do |result, (parent_key, children)|
-      result.merge(condition_hash(parent_key, children))
-    end
-    .merge(node[:parents])
-  end
-
-private
-  def condition_hash(parent_key, children)
-    prefix = "#{parent_key}/" if parent_key.in?(['ua', 'en'])
-    Hash[children.map { |key, value| ["#{parent_key}/#{key}", "#{prefix}#{value}"] }]
-  end
-end
-
 Vk::Application.routes.draw do
   concern :apartments_concern do
     root 'main_pages#show'
