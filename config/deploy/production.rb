bccb2ebb8f6871a6fac968306d1b06ec8e3647eb
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-05-11 17:15:04 +0300

    edit sectet_token to use env variables

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+set :stage, :production
+
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary server in each group
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,16 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Vk::Application.config.secret_key_base = 'dd2da6836002254cac82d1a02a563871344cf761285a04055158734cd631c796c7688f6934d453bdf876d6e74a690bc79c8f8d71992bba8b0032d25e136535e5'
+
+development:
+  domain_name: example.com
+  secret_key_base: dd2da6836002254cac82d1a02a563871344cf761285a04055158734cd631c796c7688f6934d453bdf876d6e74a690bc79c8f8d71992bba8b0032d25e136535e5
+
+test:
+  secret_key_base: a4ba8095e8a462903f3ea5cfeebaf74c484082de68b58100cbad0481f836cb4801d6e8c0882918a55459b298878a083bd44ccab147f1657b3384196c47bf5a11
+
+# Do not keep production secrets in the repository,
+# instead read values from the environment.
+production:
+  domain_name: <%= ENV["DOMAIN_NAME"] %>
+  secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>
